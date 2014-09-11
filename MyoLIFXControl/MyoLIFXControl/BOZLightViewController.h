d1aeb17b852417ea4b2a226dde499b787ff753a3
Ben Oztalay <boztalay@gmail.com>
2014-09-10 22:06:30 -0400

    Light control! Observers! Dynamic color view borders

--- MyoLIFXControl/MyoLIFXControl/BOZLightViewController.h
+++ MyoLIFXControl/MyoLIFXControl/BOZLightViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <LIFXKit/LIFXKit.h>
 
-@interface BOZLightViewController : UIViewController
+@interface BOZLightViewController : UIViewController <LFXLightObserver>
 
 @property (weak, nonatomic) IBOutlet UIView *colorView;
 @property (weak, nonatomic) IBOutlet UISlider *redSlider;
@@ -17,5 +17,8 @@
 @property (weak, nonatomic) IBOutlet UISlider *blueSlider;
 
 @property (strong, nonatomic) LFXLight* light;
+
+- (IBAction)sliderValueChanged:(id)sender;
+- (IBAction)sliderValueFinishedChanging:(id)sender;
 
 @end
--- MyoLIFXControl/MyoLIFXControl/BOZLightViewController.m
+++ MyoLIFXControl/MyoLIFXControl/BOZLightViewController.m
@@ -10,18 +10,68 @@
 
 @implementation BOZLightViewController
 
+#pragma mark - Init
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.colorView.layer.cornerRadius = 6.0f;
+    self.colorView.layer.borderWidth = 1.0f;
+    self.colorView.layer.borderColor = [UIColor clearColor].CGColor;
+    
+    [self updateColorViewBorder];
 }
 
 - (void)setLight:(LFXLight *)light
 {
+    [self.light removeLightObserver:self];
     _light = light;
-    [self.light setColor:[LFXHSBKColor colorWithHue:137.0f saturation:1.0f brightness:1.0f]];
+    [self.light addLightObserver:self];
     
     [self updateAll];
 }
+
+#pragma mark - The Light
+
+- (void)light:(LFXLight *)light didChangeColor:(LFXHSBKColor *)color
+{
+    [self updateColorViewFromLight];
+    [self updateSlidersFromLight];
+}
+
+- (void)light:(LFXLight *)light didChangeLabel:(NSString *)label
+{
+    [self updateTitle];
+}
+
+- (void)updateLightColorFromSliders
+{
+    UIColor* colorFromSliders = [self getColorFromSliders];
+    CGFloat hue, saturation, brightness, alpha;
+    [colorFromSliders getHue:&hue saturation:&saturation brightness:&brightness alpha:&alpha];
+    
+    [self.light setColor:[LFXHSBKColor colorWithHue:(hue * 360.0f) saturation:saturation brightness:brightness]];
+}
+
+- (UIColor*)getColorFromSliders
+{
+    return [UIColor colorWithRed:self.redSlider.value green:self.greenSlider.value blue:self.blueSlider.value alpha:1.0f];
+}
+
+#pragma mark - Sliders
+
+- (IBAction)sliderValueChanged:(id)sender
+{
+    [self updateColorViewFromSliders];
+}
+
+- (IBAction)sliderValueFinishedChanging:(id)sender
+{
+    [self updateLightColorFromSliders];
+}
+
+#pragma mark - Updating Views
 
 - (void)updateAll
 {
@@ -29,20 +79,51 @@
         return;
     }
     
-    // Title
-    
+    [self updateTitle];
+    [self updateColorViewFromLight];
+    [self updateSlidersFromLight];
+}
+
+- (void)updateTitle
+{
     if(self.light.label.length > 0) {
         self.title = self.light.label;
     } else {
         self.title = self.light.deviceID;
     }
-    
-    // Color
-    
+}
+
+- (void)updateColorViewFromLight
+{
     UIColor* lightColor = [self.light.color UIColor];
     self.colorView.backgroundColor = lightColor;
+    [self updateColorViewBorder];
+}
 
+- (void)updateColorViewFromSliders
+{
+    self.colorView.backgroundColor = [self getColorFromSliders];
+    [self updateColorViewBorder];
+}
+
+- (void)updateColorViewBorder
+{
+    CGFloat hue, saturation, brightness, alpha;
+    [self.colorView.backgroundColor getHue:&hue saturation:&saturation brightness:&brightness alpha:&alpha];
+    
+    CGFloat colorViewBorderAlpha = 1.0f - saturation;
+    if(saturation < 0.001f && brightness < 0.001f) {
+        colorViewBorderAlpha = 0.0f;
+    }
+    
+    self.colorView.layer.borderColor = [UIColor colorWithWhite:0.85f alpha:colorViewBorderAlpha].CGColor;
+}
+
+- (void)updateSlidersFromLight
+{
+    UIColor* lightColor = [self.light.color UIColor];
     const CGFloat* colorComponents = CGColorGetComponents(lightColor.CGColor);
+
     self.redSlider.value = colorComponents[0];
     self.greenSlider.value = colorComponents[1];
     self.blueSlider.value = colorComponents[2];
