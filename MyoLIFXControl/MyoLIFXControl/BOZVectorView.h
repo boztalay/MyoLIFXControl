4440342a8addae42bc9312d10d9354331b686ef3
Ben Oztalay <boztalay@gmail.com>
2014-10-19 20:38:34 -0400

    Messing with motion data

---
+++ MyoLIFXControl/MyoLIFXControl/BOZVectorView.h
@@ -0,0 +1,13 @@
+//
+//  BOZVectorView.h
+//  MyoLIFXControl
+//
+//  Created by Ben Oztalay on 10/19/14.
+//  Copyright (c) 2014 Ben Oztalay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOZVectorView : UIView
+
+@end
---
+++ MyoLIFXControl/MyoLIFXControl/BOZVectorView.m
@@ -0,0 +1,21 @@
+//
+//  BOZVectorView.m
+//  MyoLIFXControl
+//
+//  Created by Ben Oztalay on 10/19/14.
+//  Copyright (c) 2014 Ben Oztalay. All rights reserved.
+//
+
+#import "BOZVectorView.h"
+
+@implementation BOZVectorView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
